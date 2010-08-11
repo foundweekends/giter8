e5a7f064546696059309361cbd782904b3ce6eec
Nathan Hamblen <nathan@technically.us>
2010-08-10 23:14:30 -0400

    copy to local file

--- src/main/scala/giter8.scala
+++ src/main/scala/giter8.scala
@@ -5,13 +5,19 @@ class Giter8 extends xsbti.AppMain {
   import dispatch.liftjson.Js._
   import net.liftweb.json.JsonAST._
 
+  val re = "^src/main/g8/(.+)$".r
+  
   def run(config: xsbti.AppConfiguration) = {
     for {
       repo <- config.arguments.headOption map { _ + ".g8" }
-      blob <- http(gh / "blob" / "all" / repo / "master" ># ('blobs ? obj))
+      blobs <- http(gh / "blob" / "all" / repo / "master" ># ('blobs ? obj))
+      JField(name, JString(value)) <- blobs
+      m <- re.findFirstMatchIn(name)
     } {
-      val JField(name, JString(value)) = blob
-      println(http(gh / "blob" / "show" / repo / value as_str))
+      import java.io.{File,FileOutputStream}
+      val f = new File(m.group(1))
+      new File(f.getParent).mkdirs()
+      http(gh / "blob" / "show" / repo / value >>> new FileOutputStream(f))
     }
     new Exit(1)
   }
