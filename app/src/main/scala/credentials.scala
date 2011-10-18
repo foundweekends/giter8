d4c5641a09b3c7616cafc19b4119532d356494e9
e.e d3si9n <eed3si9n@gmail.com>
2011-10-18 19:09:22 -0400

    follow the gitnub credential convention. fixes #10

--- app/src/main/scala/credentials.scala
+++ app/src/main/scala/credentials.scala
@@ -3,15 +3,19 @@ package giter8
 trait Credentials { self: Apply =>
   def withCredentials(req: dispatch.Request) =
     credentials map { case (user, pass) => req as_! (user, pass) } getOrElse req
-
-  lazy val credentials = {
-    val props = Some(new java.io.File(System.getProperty("user.home"), ".gh")) filter {
-      _.exists
-    } map { f => readProps(new java.io.FileInputStream(f)) } getOrElse Map.empty
-    props.get("username") flatMap { user =>
-      props.get("token") map { token => (user + "/token", token) } orElse {
-        props.get("password") map { pass => (user, pass) }
-      }
+  
+  lazy val credentials: Option[(String, String)] =
+    gitConfig("github.user") flatMap { user =>
+      gitConfig("github.token") map { token =>
+        (user + "/token", token)
+      }    
     }
-  }
+  
+  // https://github.com/defunkt/gist/blob/master/lib/gist.rb#L237
+  def gitConfig(key: String): Option[String] =
+    Option(System.getenv(key.toUpperCase.replaceAll("""\.""", "_"))) map { Some(_) } getOrElse {
+      val p = new java.lang.ProcessBuilder("git", "config", "--global", key).start()
+      val reader = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream))
+      Option(reader.readLine)
+    }
 }
