9a108eeb16bb4659c17821d4941c0c442651d58d
Nathan Hamblen <nathan@technically.us>
2011-12-19 01:25:59 -0500

    adapted to work off ls master

--- project/build.scala
+++ project/build.scala
@@ -4,9 +4,13 @@ object Builds extends sbt.Build {
   import Keys._
   import ls.Plugin.{lsSettings,LsKeys}
 
+  val g8version = "0.3.2"
+
   lazy val buildSettings = Defaults.defaultSettings ++ lsSettings ++ Seq(
-    version := "0.3.2",
     organization := "net.databinder",
+    publishTo := Some("Scala Tools Nexus" at 
+      "http://nexus.scala-tools.org/content/repositories/releases/"),
+    version := g8version,
     scalaVersion := "2.9.1",
     libraryDependencies ++= Seq(
       "org.clapper" %% "scalasti" % "0.5.5"),
@@ -14,31 +18,28 @@ object Builds extends sbt.Build {
     publishArtifact in (Test, packageBin) := false,
     publishArtifact in (Compile, packageDoc) := false,
     publishArtifact in (Compile, packageSrc) := false,
-    publishTo <<= version { (v: String) =>
-      val nexus = "http://nexus.scala-tools.org/content/repositories/"
-      if(v endsWith "-SNAPSHOT") Some("Scala Tools Nexus" at nexus + "snapshots/")
-      else Some("Scala Tools Nexus" at nexus + "releases/")
-    },
-    resolvers += Resolver.url("databinder repo", url("http://databinder.net/repo"))(pattern),
-    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),    
-    publishMavenStyle := true
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
   )
-  val pattern = Patterns(false, "[organisation]/[module]/[revision]/[type]s/[module](-[classifier]).[ext]")
   
   // posterous title needs to be giter8, so both app and root are named giter8
   lazy val root = Project("root", file("."),
     settings = buildSettings ++ Seq(
       name := "giter8",
       LsKeys.skipWrite := true
-    )) aggregate(app, plugin, lsLibrary)
+    )) aggregate(plugin, app, lsLibrary)
+
   lazy val app = Project("app", file("app"),
     settings = buildSettings ++ conscript.Harness.conscriptSettings ++ Seq(
       description :=
         "Command line tool to apply templates defined on github",
+      version in lsLibrary <<= version,
+      organization in lsLibrary := "net.databinder.giter8",
+      publishTo in lsLibrary <<= publishTo,
       name := "giter8",
       libraryDependencies +=
         "net.databinder" %% "dispatch-lift-json" % "0.8.5"
-    )) dependsOn lsLibrary
+    )) dependsOn (lsLibrary)
+
   lazy val plugin = Project("giter8-plugin", file("plugin"),
     settings = buildSettings ++ Seq(
       description :=
@@ -51,5 +52,5 @@ object Builds extends sbt.Build {
       }
     ))
   lazy val lsLibrary =
-    ProjectRef(uri("git://github.com/softprops/ls.git#f6adc57"), "library")
+    ProjectRef(uri("git://github.com/softprops/ls.git#cd84ac3"), "library")
 }
