a3e88aba0638e792163c13442cf2d6d802f26900
Nathan Hamblen <nathan@technically.us>
2012-04-26 08:29:53 -0400

    add note to oauth token request

--- app/src/main/scala/authorize.scala
+++ app/src/main/scala/authorize.scala
@@ -2,7 +2,8 @@ package giter8
 
 import dispatch._
 import dispatch.liftjson.Js._
-import net.liftweb.json.JsonAST._
+import net.liftweb.json._
+import net.liftweb.json.JsonDSL._
 
 object Authorize {
   val authorizations = :/("api.github.com").secure / "authorizations"
@@ -12,7 +13,10 @@ trait Authorize { self: Giter8 =>
   import Authorize._
   
   def auth(user: String, pass: String): Either[String, String] =
-    http x (authorizations.POST.as_!(user, pass) << "{}" ># { _ \ "token" match {
+    http x (authorizations.POST.as_!(user, pass) << compact(render(
+          ("note" -> "Giter8") ~
+          ("note_url" -> "https://github.com/n8han/giter8")
+    )) ># { _ \ "token" match {
       case JString(tok) => Some(tok)
       case _ => None
     }}) {
