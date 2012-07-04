fc5c9916dee7923331db48807298196cc8b498e6
jto <jtournay@linkedin.com>
2012-07-04 08:32:53 -0700

    first scaffolding test

--- scaffold/src/main/scala/scaffolding-plugin.scala
+++ scaffold/src/main/scala/scaffolding-plugin.scala
@@ -8,15 +8,16 @@ object ScaffoldPlugin extends sbt.Plugin {
 
   object ScaffoldingKeys {
     lazy val templatesPath = SettingKey[String]("g8-templates-path")
-    lazy val scaffold    = TaskKey[String]("g8-scaffold")
+    lazy val scaffold    = TaskKey[Either[String, String]]("g8-scaffold")
   }
 
   import ScaffoldingKeys._
 
   val scafffoldTask = scaffold <<= (baseDirectory, templatesPath) map { (b, t) =>
     val folder = b / t
-    println(folder)
-    "BOOM"
+    // TODO: how to handle packages names in java (folder structure)
+    // Template hooks? package(toto.tutu) like ls(,,)
+    G8Helpers.applyRaw(folder / "controller", b, Nil)
   }
 
   lazy val scaffoldSettings: Seq[sbt.Project.Setting[_]] = Seq(
