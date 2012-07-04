70cc74b9d693c5b4d877a9b024230bd0e8cfdd1b
jto <jtournay@linkedin.com>
2012-07-04 06:35:26 -0700

    remove dead code

--- app/src/main/scala/apply.scala
+++ app/src/main/scala/apply.scala
@@ -1,27 +1,6 @@
 package giter8
 
-case class FileInfo(name: String, hash: String, mode: String)
-
-trait Apply extends GitRepo { self: Giter8 =>
-  import dispatch._
-  import dispatch.liftjson.Js._
-  import net.liftweb.json.JsonAST._
-  import scala.collection.JavaConversions._
-  import java.io.{File,FileWriter,FileOutputStream}
-  import scala.util.control.Exception.allCatch
-
-  def setFileMode(f: File, mode: String) = allCatch opt {
-    if ((mode(3).toString.toInt & 0x1) > 0) {
-      f.setExecutable(true)
-    }
-  }
-
-  private def use[C <: { def close(): Unit }, T](c: C)(f: C => T): T =
-    try { f(c) } finally { c.close() }
-}
-
-trait GitRepo { self: Giter8 =>
-
+trait Apply { self: Giter8 =>
   import java.io.File
   import org.eclipse.jgit.api._
   import scala.util.control.Exception.{allCatch,catching}
