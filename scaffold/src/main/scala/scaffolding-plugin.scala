4665fbe8cd8634355337a4f115d41de765b6f63f
jto <jtournay@linkedin.com>
2012-07-06 07:15:43 -0700

    completion for 1 param

--- scaffold/src/main/scala/scaffolding-plugin.scala
+++ scaffold/src/main/scala/scaffolding-plugin.scala
@@ -12,30 +12,48 @@ object ScaffoldPlugin extends sbt.Plugin {
   }
 
   import ScaffoldingKeys._
-
   import complete._
-  import complete.DefaultParsers._
 
-  val parser: sbt.Project.Initialize[State => Parser[String]] = 
+  val parser: sbt.Project.Initialize[State => Parser[(File, Map[String, String])]] =
     (baseDirectory, templatesPath) { (b, t) =>
+
+      import complete.DefaultParsers._
+      import java.io.FileInputStream
+      
       (state: State) =>
         val folder = b / t
-        val templates = folder.listFiles
+
+        val templatesParsers = folder.listFiles
           .filter(f => f.isDirectory && !f.isHidden)
           .map(_.getName: Parser[String])
+          .reduceLeft(_ | _)
 
-        Space ~> templates.reduceLeft(_ | _)
+        val eq: Parser[String] = "="
+
+        Space ~> templatesParsers.map(folder / _).flatMap { root =>
+
+          val validParam = GIO.readProps(new FileInputStream(root / "default.properties"))
+            .keys
+            .map(x => x: Parser[String])
+            .reduceLeft(_ | _)
+
+          val p = ((validParam <~ eq): Parser[String]) ~ NotSpace
+
+          Space ~> (p?) map { param => 
+            (root, param.map(Map(_)).getOrElse(Map.empty))
+          }
+        }
     }
 
-  val scafffoldTask = scaffold <<= InputTask(parser){ (argTask: TaskKey[String]) =>
-    (baseDirectory, templatesPath, argTask) map { (b, t, name) =>
-      val folder = b / t
+  val scafffoldTask = scaffold <<= InputTask(parser){ (templateData: TaskKey[(File, Map[String, String])]) =>
+    (baseDirectory, templateData) map { (b, data) =>
+      val (t, param) = data
       // TODO: how to handle packages names in java (folder structure)
       // Template hooks? package(toto.tutu) like ls(,,)
-      G8Helpers.applyRaw(folder / name, b, Nil).fold(
-        e => error(e),
-        r => println("Success :)")
-      )
+      // G8Helpers.applyRaw(t, b, param).fold(
+      //         e => error(e),
+      //         r => println("Success :)")
+      //       )
     }
   }
 
