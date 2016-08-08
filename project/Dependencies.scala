f96ef01f3e4c64044c038207511aa2f9320fe73f
Eugene Yokota <eed3si9n@gmail.com>
2016-08-07 22:08:56 -0400

    Use Gigahorse 0.1.1 instead of Dispatch

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,6 @@ import sbt._
 object Dependencies {
   val scalasti = "org.clapper" %% "scalasti" % "2.0.0"
   val jline = ("jline" % "jline" % "1.0" force)
-  val lsCore = "me.lessis" %% "ls" % "0.1.3"
-  // override ls's older version of dispatch
-  val dispatchCore = "net.databinder.dispatch" %% "dispatch-core" % "0.11.2"
   val commonsIo = "commons-io" % "commons-io" % "2.4"
   val plexusArchiver = "org.codehaus.plexus" % "plexus-archiver" % "2.2" excludeAll(
     ExclusionRule("org.apache.commons", "commons-compress"),
@@ -15,7 +12,7 @@ object Dependencies {
   )
   val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "1.3.0.201202151440-r"
   val scopt = "com.github.scopt" %% "scopt" % "3.1.0"
-  val dispatchHttp = "net.databinder" %% "dispatch-http" % "0.8.10"
+  val gigahorseCore = "com.eed3si9n" %% "gigahorse-core" % "0.1.1"
   val scalacheck = "org.scalacheck" %% "scalacheck" % "1.13.0"
   val sbtIo = "org.scala-sbt" %% "io" % "1.0.0-M6"
 }
