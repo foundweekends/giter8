4c2a04176ed310586370540d6d2b43d8a761a015
Eugene Yokota <eed3si9n@gmail.com>
2016-08-14 01:25:08 -0400

    Giter8Plugin
    This changes the plugin to an auto plugin named Giter8Plugin. As
    demonstrated in plugin/src/sbt-test/giter8/root-layout/ test this is
    designed to work with the root layout as well.

--- library/src/main/scala/g8.scala
+++ library/src/main/scala/g8.scala
@@ -52,6 +52,41 @@ object G8 {
     */
   type ValueF = ResolvedProperties => String
 
+  // Called from JgitHelper
+  def fromDirectory(baseDirectory: File, outputDirectory: File,
+    arguments: Seq[String], forceOverwrite: Boolean): Either[String, String] =
+    G8Helpers.applyT((file: File) =>
+      G8Helpers.fetchInfo(file: File,
+        defaultTemplatePaths,
+        List(path("src") / "main" / "scaffolds",
+          path("project") / "src" / "main" / "scaffolds"))
+    )(baseDirectory, outputDirectory, arguments, forceOverwrite)
+
+  val defaultTemplatePaths: List[Path] = List(path("src") / "main" / "g8", Path(Nil))
+
+  def apply(fromMapping: Seq[(File,String)], toPath: File, parameters: Map[String,String]): Seq[File] =
+    fromMapping filter { !_._1.isDirectory } flatMap { case (in, relative) =>
+      apply(in, expandPath(relative, toPath, parameters), parameters)
+    }
+
+  def apply(in: File, out: File, parameters: Map[String,String]) = {
+    try {
+      if (verbatim(in, parameters)) FileUtils.copyFile(in, out)
+      else {
+        write(in, out, parameters/*, false*/)
+      }
+    }
+    catch {
+      case e: Exception =>
+        println("Falling back to file copy for %s: %s" format(in.toString, e.getMessage))
+        FileUtils.copyFile(in, out)
+    }
+    allCatch opt {
+      if (in.canExecute) out.setExecutable(true)
+    }
+    Seq(out)
+  }
+
   def applyTemplate(default: String, resolved: ResolvedProperties): String = {
     val group = STGroup('$', '$')
     group.nativeGroup.setListener(new STErrorHandler)
@@ -89,29 +124,6 @@ object G8 {
 
   private val renderer = new StringRenderer
 
-  def apply(fromMapping: Seq[(File,String)], toPath: File, parameters: Map[String,String]): Seq[File] =
-    fromMapping filter { !_._1.isDirectory } flatMap { case (in, relative) =>
-      apply(in, expandPath(relative, toPath, parameters), parameters)
-    }
-
-  def apply(in: File, out: File, parameters: Map[String,String]) = {
-    try {
-      if (verbatim(in, parameters)) FileUtils.copyFile(in, out)
-      else {
-        write(in, out, parameters/*, false*/)
-      }
-    }
-    catch {
-      case e: Exception =>
-        println("Falling back to file copy for %s: %s" format(in.toString, e.getMessage))
-        FileUtils.copyFile(in, out)
-    }
-    allCatch opt {
-      if (in.canExecute) out.setExecutable(true)
-    }
-    Seq(out)
-  }
-
   def write(in: File, out: File, parameters: Map[String, String]/*, append: Boolean*/) {
     try {
       Option(PlexusIoResourceAttributeUtils.getFileAttributes(in)) match {
@@ -147,17 +159,26 @@ object G8 {
     case '.' => """\."""
     case x => x.toString
   }).r
-  def expandPath(relative: String, toPath: File, parameters: Map[String,String]): File = {
-    val fileParams = Map(parameters.toSeq map {
-      case (k, v) if k == "package" => (k, v.replaceAll("""\.""", System.getProperty("file.separator") match {
-          case "\\"  => "\\\\"
-          case sep => sep
-        }))
-      case x => x
-    }: _*)
+  def expandPath(relative: String, toPath: File, parameters: Map[String,String]): File =
+    try {
+      val fileParams = Map(parameters.toSeq map {
+        case (k, v) if k == "package" => (k, v.replaceAll("""\.""", System.getProperty("file.separator") match {
+            case "\\"  => "\\\\"
+            case sep => sep
+          }))
+        case x => x
+      }: _*)
 
-    new File(toPath, applyTemplate(formatize(relative), fileParams))
-  }
+      new File(toPath, applyTemplate(formatize(relative), fileParams))
+    }
+    catch {
+      case e : STException =>
+        // add the current relative path to the exception for debugging purposes
+        throw new STException(s"relative: $relative, toPath: $toPath, ${e.getMessage}", null)
+      case t : Throwable =>
+        throw t
+    }
+
   private def formatize(s: String) = s.replaceAll("""\$(\w+)__(\w+)\$""", """\$$1;format="$2"\$""")
 
   def decapitalize(s: String) = if (s.isEmpty) s else s(0).toLower + s.substring(1)
@@ -171,6 +192,44 @@ object G8 {
   def packageDir(s: String) = s.replace(".", System.getProperty("file.separator"))
   def addRandomId(s: String) = s + "-" + new java.math.BigInteger(256, new java.security.SecureRandom).toString(32)
 
+  val Param = """^--(\S+)=(.+)$""".r
+  implicit class RichFile(file: File) {
+    def /(child: String): File = new File(file, child)
+    def /(path: Path): File = (file /: path.paths) { _ / _ }
+  }
+  def file(path: String): File = new File(path)
+  def path(path: String): Path = Path(List(path))
+
+  private[giter8] def getFiles(filter: File => Boolean)(f: File): Stream[File] =
+    if (f.isDirectory) f.listFiles().toStream.filter(filter).flatMap(getFiles(filter))
+    else if (filter(f)) Stream(f)
+    else Stream()
+  /** Select the root template directory from the given relative paths. */
+  def templateRoot(baseDirectory: File, templatePaths: List[Path]): File =
+    // Go through the list of dirs and pick the first one.
+    (templatePaths map {
+      case Path(Nil) => baseDirectory
+      case p         => baseDirectory / p
+    }).find(_.exists).getOrElse(baseDirectory)
+
+  /** Extract template files under the first matching relative templatePaths under the baseDirectory. */
+  def templateFiles(root: File, baseDirectory: File): Stream[File] =
+    {
+      val gitFiles = getFiles(_ => true)(baseDirectory / ".git").toSet
+      val targetFiles =
+        getFiles(_ => true)(baseDirectory / "target").toSet ++
+        getFiles(_ => true)(baseDirectory / "project" / "target").toSet ++
+        getFiles(_ => true)(baseDirectory / "project" / "project" / "target").toSet
+      val metaDir = baseDirectory / "project"
+      def baseAndMeta(xs: String*): Set[File] =
+        xs.toSet flatMap { x: String => Set( baseDirectory / x, metaDir / x ) }
+      val pluginFiles = baseAndMeta("giter8.sbt", "g8.sbt")
+      val metadata = baseAndMeta("activator.properties", "template.properties")
+      val testFiles = baseAndMeta("test", "giter8.test", "g8.test")
+      // .git and other files
+      val skipFiles: Set[File] = gitFiles ++ pluginFiles ++ metadata ++ testFiles ++ targetFiles
+      getFiles(!skipFiles(_))(root)
+    }
 }
 
 case class Config(
@@ -188,15 +247,7 @@ object G8Helpers {
   import scala.util.control.Exception.catching
   import G8._
 
-  val Param = """^--(\S+)=(.+)$""".r
-  implicit class RichFile(file: File) {
-    def /(child: String): File = new File(file, child)
-    def /(path: Path): File = (file /: path.paths) { _ / _ }
-  }
-  def file(path: String): File = new File(path)
-  def path(path: String): Path = Path(List(path))
-
-  private def applyT(
+  private[giter8] def applyT(
     fetch: File => Either[String, (UnresolvedProperties, Stream[File], File, Option[File])]
     // isScaffolding: Boolean = false
   )(
@@ -229,19 +280,10 @@ object G8Helpers {
         Left("Unknown exception: " + t.getMessage)
     }
 
-  private def fetchProjectTemplateinfo(file: File) =
-    fetchInfo(file: File, List(path("src") / "main" / "g8", Path(Nil)),
-      List(path("src") / "main" / "scaffolds",
-        path("project") / "src" / "main" / "scaffolds"))
-
   private def fetchRawTemplateinfo(file: File) =
     fetchInfo(file, List(Path(Nil)), Nil)
 
-  def applyTemplate = applyT(fetchProjectTemplateinfo) _
   def applyRaw = applyT(fetchRawTemplateinfo) _
-
-  private def getFiles(filter: File => Boolean)(f: File): Stream[File] =
-    f #:: (if (f.isDirectory) f.listFiles().toStream.filter(filter).flatMap(getFiles(filter)) else Stream.empty)
 
   private def getVisibleFiles = getFiles(!_.isHidden) _
 
@@ -253,29 +295,12 @@ object G8Helpers {
   /**
   * Extract params, template files, and scaffolding folder based on the conventionnal project structure
   */
-  private def fetchInfo(
-    baseDirectory: File,
-    templatePaths: List[Path],
-    scaffoldPaths: List[Path]
-  ) = {
+  private[giter8] def fetchInfo(baseDirectory: File,
+    templatePaths: List[Path], scaffoldPaths: List[Path]): Either[String, (UnresolvedProperties, Stream[File], File, Option[File])] = {
     import java.io.FileInputStream
-    // Go through the list of dirs and pick the first one.
-    val templatesRoot =
-      (templatePaths map {
-        case Path(Nil) => baseDirectory
-        case p         => baseDirectory / p
-      }).find(_.exists).getOrElse(baseDirectory)
-
-    val gitFiles = getFiles(_ => true)(baseDirectory / ".git").toSet
+    val templatesRoot = templateRoot(baseDirectory, templatePaths)
+    val fs = templateFiles(templatesRoot, baseDirectory)
     val metaDir = baseDirectory / "project"
-    def baseAndMeta(xs: String*): Set[File] =
-      xs.toSet flatMap { x: String => Set( baseDirectory / x, metaDir / x ) }
-    val pluginFiles = baseAndMeta("giter8.sbt", "g8.sbt")
-    val metadata = baseAndMeta("activator.properties", "template.properties")
-    val testFiles = baseAndMeta("test", "giter8.test", "g8.test")
-    // .git and other files
-    val skipFiles: Set[File] = gitFiles ++ pluginFiles ++ metadata ++ testFiles
-    val fs = getFiles(!skipFiles(_))(templatesRoot)
     val propertiesLoc0 = templatesRoot / "default.properties"
     val propertiesLoc1 = metaDir / "default.properties"
     val propertiesLocs = Set(propertiesLoc0, propertiesLoc1)
---
+++ plugin/src/main/scala/Giter8Plugin.scala
@@ -0,0 +1,122 @@
+/*
+ * Original implementation (C) 2010-2015 Nathan Hamblen and contributors
+ * Adapted and extended in 2016 by foundweekends project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package giter8
+
+import sbt._
+import ScriptedPlugin._
+
+object Giter8Plugin extends sbt.AutoPlugin {
+  override val requires = sbt.plugins.JvmPlugin
+  override val trigger = allRequirements
+
+  import Keys._
+  import scala.io.Source
+
+  object autoImport {
+    lazy val g8                = taskKey[Seq[File]]("Apply default parameters to input templates and write to output.")
+    lazy val g8PropertiesFile  = settingKey[File]("g8-properties-file")
+    lazy val g8Properties      = taskKey[Map[String, String]]("g8-properties")
+    lazy val g8TestScript      = settingKey[File]("g8-test-script")
+    lazy val g8Test            = inputKey[Unit]("Run `sbt test` in output to smoke-test the templates")
+  }
+
+  import autoImport._
+
+  lazy val baseGiter8Settings: Seq[Def.Setting[_]] = Seq(
+    g8 := {
+      val base = (unmanagedSourceDirectories in g8).value
+      val srcs = (sources in g8).value
+      val out = (target in g8).value
+      val props = (g8Properties in g8).value
+      val s = streams.value
+      IO.delete(out)
+      G8(srcs pair relativeTo(base), out, props)
+    },
+    aggregate in g8 := false,
+    unmanagedSourceDirectories in g8 := {
+      val dir1 = (sourceDirectory.value / "g8").get
+      if (dir1.nonEmpty) dir1
+      else List(baseDirectory.value)
+    },
+    sources in g8 := {
+      val dirs = (unmanagedSourceDirectories in g8).value
+      val root = dirs.head
+      G8.templateFiles(root, baseDirectory.value)
+    },
+    target in g8 := { target.value / "g8" },
+    g8PropertiesFile in g8 := {
+      val propertiesLoc0 = ((unmanagedSourceDirectories in g8).value / "default.properties").get.headOption
+      val propertiesLoc1: Option[File] = Some((baseDirectory in LocalRootProject).value / "project" / "default.properties")
+      (propertiesLoc0 orElse propertiesLoc1).get
+    },
+    g8Properties in g8 := {
+      val f = (g8PropertiesFile in g8).value
+      if (f.exists) {
+        val in = new java.io.FileInputStream(f)
+        try {
+          G8Helpers.transformProps(G8Helpers.readProps(in)).fold(
+            err => sys.error(err),
+            _.foldLeft(G8.ResolvedProperties.empty) { case (resolved, (k, v)) =>
+              resolved + (k -> G8.DefaultValueF(v)(resolved))
+            }.toMap
+          )
+        } finally {
+          in.close
+        }
+      }
+      else Map.empty
+    }
+  )
+
+  lazy val giter8TestSettings: Seq[Def.Setting[_]] = scriptedSettings ++ Seq(
+    g8Test in Test := { scriptedTask.evaluated },
+    aggregate in (Test, g8Test) := false,
+    scriptedDependencies := {
+      val x = (g8 in Test).value
+    },
+    g8 in Test := {
+      val base = (unmanagedSourceDirectories in (Compile, g8)).value
+      val srcs = (sources in (Compile, g8)).value
+      val out = (target in (Test, g8)).value
+      val props = (g8Properties in (Test, g8)).value
+      val ts = (g8TestScript in (Test, g8)).value
+      val s = streams.value
+      IO.delete(out)
+      val retval = G8(srcs pair relativeTo(base), out, props)
+
+      // copy test script or generate one
+      val script = new File(out, "test")
+      if (ts.exists) IO.copyFile(ts, script)
+      else IO.write(script, """>test""")
+      retval :+ script
+    },
+    sbtTestDirectory := { target.value / "sbt-test" },
+    target in (Test, g8) := { sbtTestDirectory.value / name.value / "scripted" },
+    g8TestScript := {
+      val dir = (sourceDirectory in Test).value
+      val metadir = (baseDirectory in LocalRootProject).value / "project"
+      val file0 = dir / "g8" / "test"
+      val files = List(file0, dir / "g8" / "giter8.test", dir / "g8" / "g8.test",
+        metadir / "test", metadir / "giter8.test", metadir / "g8.test")
+      files.find(_.exists).getOrElse(file0)
+    },
+    scriptedBufferLog in (Test, g8) := true
+  )
+
+  override lazy val projectSettings: Seq[Def.Setting[_]] = inConfig(Compile)(baseGiter8Settings) ++ giter8TestSettings
+}
