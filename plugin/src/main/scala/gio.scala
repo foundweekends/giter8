a41daeaa76f532332198f1e3e8218813f29dc7e1
xuwei-k <6b656e6a69@gmail.com>
2017-01-29 22:08:52 +0900

    use JavaConverters instead of JavaConversions
    JavaConversions is deprecated since Scala 2.12
    https://github.com/scala/scala/blob/v2.12.1/src/library/scala/collection/JavaConversions.scala#L59

--- plugin/src/main/scala/gio.scala
+++ plugin/src/main/scala/gio.scala
@@ -20,11 +20,11 @@ package giter8
 object GIO {
 
   def readProps(stm: java.io.InputStream) = {
-    import scala.collection.JavaConversions._
+    import scala.collection.JavaConverters._
     val p = new java.util.Properties
     p.load(stm)
     stm.close()
-    (Map.empty[String, String] /: p.propertyNames) { (m, k) =>
+    p.propertyNames.asScala.foldLeft(Map.empty[String, String]) { (m, k) =>
       m + (k.toString -> p.getProperty(k.toString))
     }
   }
