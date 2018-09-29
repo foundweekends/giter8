ee6370f411df6a101c74b339af337434cbc9f822
KOUNOIKE <kounoike.yuusuke@gmail.com>
2018-09-30 04:03:05 +0900

    use https

--- library/src/main/scala/giter8/MavenHelper.scala
+++ library/src/main/scala/giter8/MavenHelper.scala
@@ -25,7 +25,7 @@ import org.apache.http.impl.client.DefaultHttpClient
 // http://hc.apache.org/httpcomponents-client-4.2.x/httpclient/apidocs/
 trait MavenHelper {
   def fromMaven(org: String, name: String, getVersions: Boolean)(process: (String, NodeSeq) => VersionE): VersionE = {
-    val search = "http://search.maven.org/solrsearch/select"
+    val search = "https://search.maven.org/solrsearch/select"
     val loc    = s"""$search?q=g:%22$org%22+AND+a:%22$name%22&rows=10&wt=xml${if (getVersions) "&core=gav" else ""}"""
 
     withHttp(loc) { response =>
