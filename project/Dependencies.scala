e87298e4d04bd7f5f0d409b957c79f3501da2619
xuwei-k <6b656e6a69@gmail.com>
2026-07-26 07:49:04 +0900

    sbt 2.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,9 +24,9 @@ object Dependencies {
   val sbtIo                                  = "org.scala-sbt" %% "io" % "1.12.2"
   val scala212                               = "2.12.21"
   val scala213                               = "2.13.18"
-  val scala3                                 = "3.7.4"
+  val scala3                                 = "3.8.4"
   val sbt1                                   = "1.3.13"
-  val sbt2                                   = "2.0.0-RC8"
+  val sbt2                                   = "2.0.3"
   val scalaXml                               = "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
   def parserCombinator(scalaVersion: String) = "org.scala-lang.modules" %% "scala-parser-combinators" % {
     CrossVersion.partialVersion(scalaVersion) match {
